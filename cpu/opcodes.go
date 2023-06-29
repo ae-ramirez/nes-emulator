7759ad27b22e431e86be16750c76cdebc5929d67
Alejandro Ramirez <alejramirez@csumb.edu>
2023-06-28 20:28:08 -0700

    Add cmp, cpx, cpy, dec, dex, dey, eor, inc opcodes
    Still need tests for added opcodes

--- cpu/opcodes.go
+++ cpu/opcodes.go
@@ -68,6 +68,46 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 		newOpCode(0x58, "CLI", 1, 2, NoneAddressing),
 		newOpCode(0xb8, "CLV", 1, 2, NoneAddressing),
 
+		newOpCode(0xc9, "CMP", 2, 2, Immediate),
+		newOpCode(0xc5, "CMP", 2, 3, ZeroPage),
+		newOpCode(0xd5, "CMP", 2, 4, ZeroPage_X),
+		newOpCode(0xcd, "CMP", 3, 4, Absolute),
+		newOpCode(0xdd, "CMP", 3, 4 /* +1 if page crossed */, Absolute_X),
+		newOpCode(0xd9, "CMP", 3, 4 /* +1 if page crossed */, Absolute_Y),
+		newOpCode(0xc1, "CMP", 2, 6, Indirect_X),
+		newOpCode(0xd1, "CMP", 2, 5 /* +1 if page crossed */, Indirect_Y),
+
+		newOpCode(0xe0, "CPX", 2, 2, Immediate),
+		newOpCode(0xe4, "CPX", 2, 3, ZeroPage),
+		newOpCode(0xec, "CPX", 3, 4, Absolute),
+
+		newOpCode(0xc0, "CPY", 2, 2, Immediate),
+		newOpCode(0xc4, "CPY", 2, 3, ZeroPage),
+		newOpCode(0xcc, "CPY", 3, 4, Absolute),
+
+		newOpCode(0xc6, "DEC", 2, 5, ZeroPage),
+		newOpCode(0xd6, "DEC", 2, 6, ZeroPage_X),
+		newOpCode(0xce, "DEC", 3, 6, Absolute),
+		newOpCode(0xde, "DEC", 3, 7, Absolute_X),
+
+		newOpCode(0xca, "DEX", 1, 2, NoneAddressing),
+
+		newOpCode(0x88, "DEY", 1, 2, NoneAddressing),
+
+		newOpCode(0x49, "EOR", 2, 2, Immediate),
+		newOpCode(0x45, "EOR", 2, 3, ZeroPage),
+		newOpCode(0x55, "EOR", 2, 4, ZeroPage_X),
+		newOpCode(0x4d, "EOR", 3, 4, Absolute),
+		newOpCode(0x5d, "EOR", 3, 4 /* +1 if page crossed */, Absolute_X),
+		newOpCode(0x59, "EOR", 3, 4 /* +1 if page crossed */, Absolute_Y),
+		newOpCode(0x41, "EOR", 2, 6, Indirect_X),
+		newOpCode(0x51, "EOR", 2, 5 /* +1 if page crossed */, Indirect_Y),
+
+		newOpCode(0xe6, "INC", 2, 5, ZeroPage),
+		newOpCode(0xf6, "INC", 2, 6, ZeroPage_X),
+		newOpCode(0xee, "INC", 3, 6, Absolute),
+		newOpCode(0xfe, "INC", 3, 7, Absolute_X),
+
 		newOpCode(0xa9, "LDA", 2, 2, Immediate),
 		newOpCode(0xa5, "LDA", 2, 3, ZeroPage),
 		newOpCode(0xb5, "LDA", 2, 4, ZeroPage_X),
@@ -210,6 +250,72 @@ func (cpu *CPU) clv() {
 	cpu.setStatusFlag(OverflowFlag, false)
 }
 
+func (cpu *CPU) cmp(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+
+	cpu.setStatusFlag(CarryFlag, cpu.register_a >= val)
+	cpu.setStatusFlag(ZeroFlag, cpu.register_a == val)
+	cpu.setStatusFlag(NegativeFlag, (cpu.register_a-val)>>7 == 1)
+}
+
+func (cpu *CPU) cpx(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+
+	cpu.setStatusFlag(CarryFlag, cpu.register_x >= val)
+	cpu.setStatusFlag(ZeroFlag, cpu.register_x == val)
+	cpu.setStatusFlag(NegativeFlag, (cpu.register_x-val)>>7 == 1)
+}
+
+func (cpu *CPU) cpy(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+
+	cpu.setStatusFlag(CarryFlag, cpu.register_y >= val)
+	cpu.setStatusFlag(ZeroFlag, cpu.register_y == val)
+	cpu.setStatusFlag(NegativeFlag, (cpu.register_y-val)>>7 == 1)
+}
+
+func (cpu *CPU) dec(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr) - 1
+	cpu.mem_write(addr, val)
+
+	cpu.updateZeroAndNegativeFlags(val)
+}
+
+func (cpu *CPU) dex() {
+	cpu.register_x -= 1
+	cpu.updateZeroAndNegativeFlags(cpu.register_x)
+}
+
+func (cpu *CPU) dey() {
+	cpu.register_y -= 1
+	cpu.updateZeroAndNegativeFlags(cpu.register_y)
+}
+
+func (cpu *CPU) eor(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+	cpu.register_a ^= val
+
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
+}
+
+func (cpu *CPU) inc(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr) + 1
+	cpu.mem_write(addr, val)
+
+	cpu.updateZeroAndNegativeFlags(val)
+}
+
+func (cpu *CPU) inx() {
+	cpu.register_x += 1
+	cpu.updateZeroAndNegativeFlags(cpu.register_x)
+}
+
 func (cpu *CPU) lda(mode AddressingMode) {
 	addr := cpu.getOperandAddress(mode)
 	val := cpu.mem_read(addr)
@@ -220,11 +326,6 @@ func (cpu *CPU) lda(mode AddressingMode) {
 
 func (cpu *CPU) tax() {
 	cpu.register_x = cpu.register_a
-	cpu.updateZeroAndNegativeFlags(cpu.register_x)
-}
-
-func (cpu *CPU) inx() {
-	cpu.register_x += 1
 	cpu.updateZeroAndNegativeFlags(cpu.register_x)
 }
 
