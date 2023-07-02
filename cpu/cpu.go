f62b75ede82fba20016ea37dffe3b6698e1d9822
Alejandro Ramirez <alejramirez@csumb.edu>
2023-07-01 21:29:11 -0700

    Added opcodes:
    adc, bcc, bcs, beq, bmi, bne, bpl, bvc, bvs, inx, iny, jmp, nop, ora,
    rol, ror, sbc, sec, sed, sei, stx, sty, tay, txa, tya
    
    Tests are still needed for added opcodes

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,7 @@
 package cpu
 
+import "fmt"
+
 const (
 	CarryFlag            uint8 = 0b0000_0001
 	ZeroFlag             uint8 = 0b0000_0010
@@ -63,19 +65,55 @@ func (cpu *CPU) load(program []uint8) {
 
 func (cpu *CPU) run() {
 	opcodes := cpu.opcodes()
+	var curr_program_counter uint16
 	for {
 		code := cpu.mem_read(cpu.program_counter)
 		cpu.program_counter += 1
+		curr_program_counter = cpu.program_counter
 
 		opcode := opcodes[code]
 
 		switch code {
+		case 0x69, 0x65, 0x75, 0x6d, 0x7d, 0x79, 0x61, 0x71:
+			cpu.adc(opcode.mode)
 		case 0x29, 0x25, 0x35, 0x2d, 0x3d, 0x39, 0x21, 0x31:
 			cpu.and(opcode.mode)
 		case 0x0a, 0x06, 0x16, 0x0e, 0x1e:
 			cpu.asl(opcode.mode)
+		case 0x90:
+			// bcc
+			cpu.branch(cpu.isStatusFlagClear(CarryFlag))
+		case 0xb0:
+			// bcs
+			cpu.branch(cpu.isStatusFlagSet(CarryFlag))
+		case 0xf0:
+			// beq
+			cpu.branch(cpu.isStatusFlagSet(ZeroFlag))
 		case 0x24, 0x2c:
 			cpu.bit(opcode.mode)
+		case 0x30:
+			// bmi
+			cpu.branch(cpu.isStatusFlagSet(NegativeFlag))
+		case 0xd0:
+			// bne
+			cpu.branch(cpu.isStatusFlagClear(ZeroFlag))
+		case 0x10:
+			// bpl
+			cpu.branch(cpu.isStatusFlagClear(NegativeFlag))
+		case 0x50:
+			// bvc
+			cpu.branch(cpu.isStatusFlagClear(OverflowFlag))
+		case 0x70:
+			// bvs
+			cpu.branch(cpu.isStatusFlagSet(OverflowFlag))
+		case 0x18:
+			cpu.clc()
+		case 0xd8:
+			cpu.cld()
+		case 0x58:
+			cpu.cli()
+		case 0xb8:
+			cpu.clv()
 		case 0xc9, 0xc5, 0xd5, 0xcd, 0xdd, 0xd9, 0xc1, 0xd1:
 			cpu.cmp(opcode.mode)
 		case 0xe0, 0xe4, 0xec:
@@ -92,28 +130,62 @@ func (cpu *CPU) run() {
 			cpu.eor(opcode.mode)
 		case 0xe6, 0xf6, 0xee, 0xfe:
 			cpu.inc(opcode.mode)
-		case 0xa9, 0xa5, 0xb5, 0xad, 0xbd, 0xb9, 0xa1, 0xb1:
-			cpu.lda(opcode.mode)
-		case 0x85, 0x95, 0x8d, 0x9d, 0x99, 0x81, 0x91:
-			cpu.sta(opcode.mode)
-		case 0xaa:
-			cpu.tax()
 		case 0xe8:
 			cpu.inx()
-		case 0x18:
-			cpu.clc()
-		case 0xd8:
-			cpu.cld()
-		case 0x58:
-			cpu.cli()
-		case 0xb8:
-			cpu.clv()
+		case 0xc8:
+			cpu.iny()
+		case 0x4c:
+			cpu.jmp_absolute()
+		case 0x6c:
+			cpu.jmp_indirect()
+		case 0xa9, 0xa5, 0xb5, 0xad, 0xbd, 0xb9, 0xa1, 0xb1:
+			cpu.lda(opcode.mode)
+		case 0xa2, 0xa6, 0xb6, 0xae, 0xbe:
+			cpu.ldx(opcode.mode)
+		case 0xa0, 0xa4, 0xb4, 0xac, 0xbc:
+			cpu.ldy(opcode.mode)
+		case 0x4a, 0x46, 0x56, 0x4e, 0x5e:
+			cpu.lsr(opcode.mode)
+		case 0xea:
+			// nop
+			break
+		case 0x09, 0x05, 0x15, 0x0d, 0x1d, 0x19, 0x01, 0x11:
+			cpu.ora(opcode.mode)
+		case 0x2a, 0x26, 0x36, 0x2e, 0x3e:
+			cpu.rol(opcode.mode)
+		case 0x6a, 0x66, 0x76, 0x6e, 0x7e:
+			cpu.ror(opcode.mode)
+		case 0xe9, 0xe5, 0xf5, 0xed, 0xfd, 0xf9, 0xe1, 0xf1:
+			cpu.sbc(opcode.mode)
+		case 0x38:
+			cpu.sec()
+		case 0xf8:
+			cpu.sed()
+		case 0x78:
+			cpu.sei()
+		case 0x85, 0x95, 0x8d, 0x9d, 0x99, 0x81, 0x91:
+			cpu.sta(opcode.mode)
+		case 0x86, 0x96, 0x8e:
+			cpu.stx(opcode.mode)
+		case 0x84, 0x94, 0x8c:
+			cpu.sty(opcode.mode)
+		case 0xaa:
+			cpu.tax()
+		case 0xa8:
+			cpu.tay()
+		case 0x8a:
+			cpu.txa()
+		case 0x98:
+			cpu.tya()
 		case 0x00:
 			return
 		default:
-			panic("Unsupported opcode")
+			panic(fmt.Errorf("Unsupported opcode: %#02x", code))
 		}
 
-		cpu.program_counter += uint16(opcode.len) - 1
+		// A branch didn't occur
+		if curr_program_counter == cpu.program_counter {
+			cpu.program_counter += uint16(opcode.len) - 1
+		}
 	}
 }
--- cpu/opcodes.go
+++ cpu/opcodes.go
@@ -41,9 +41,14 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 	var m map[uint8]*OpCode
 
 	OpCodes := []*OpCode{
-		newOpCode(0x00, "BRK", 1, 7, NoneAddressing),
-		newOpCode(0xaa, "TAX", 1, 7, NoneAddressing),
-		newOpCode(0xe8, "INX", 1, 2, NoneAddressing),
+		newOpCode(0x69, "ADC", 2, 2, Immediate),
+		newOpCode(0x65, "ADC", 2, 3, ZeroPage),
+		newOpCode(0x75, "ADC", 2, 4, ZeroPage_X),
+		newOpCode(0x6d, "ADC", 3, 4, Absolute),
+		newOpCode(0x7d, "ADC", 3, 4 /* +1 if page crossed */, Absolute_X),
+		newOpCode(0x79, "ADC", 3, 4 /* +1 if page crossed */, Absolute_Y),
+		newOpCode(0x61, "ADC", 2, 6, Indirect_X),
+		newOpCode(0x71, "ADC", 2, 5 /* +1 if page crossed */, Indirect_Y),
 
 		newOpCode(0x29, "AND", 2, 2, Immediate),
 		newOpCode(0x25, "AND", 2, 3, ZeroPage),
@@ -60,8 +65,19 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 		newOpCode(0x0e, "ASL", 3, 6, Absolute),
 		newOpCode(0x1e, "ASL", 3, 7, Absolute_X),
 
-		newOpCode(0x24, "BIT", 2, 3, ZeroPage),
+		newOpCode(0x90, "BCC", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0xb0, "BCS", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0xf0, "BEQ", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0x30, "BMI", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0xd0, "BNE", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0x10, "BPL", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0x50, "BVC", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+		newOpCode(0x70, "BVS", 2, 2 /* +1 if branch succeeds, +2 if to a new page */, Immediate),
+
 		newOpCode(0x2c, "BIT", 3, 4, Absolute),
+		newOpCode(0x24, "BIT", 2, 3, ZeroPage),
+
+		newOpCode(0x00, "BRK", 1, 7, NoneAddressing),
 
 		newOpCode(0x18, "CLC", 1, 2, NoneAddressing),
 		newOpCode(0xd8, "CLD", 1, 2, NoneAddressing),
@@ -108,6 +124,13 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 		newOpCode(0xee, "INC", 3, 6, Absolute),
 		newOpCode(0xfe, "INC", 3, 7, Absolute_X),
 
+		newOpCode(0xe8, "INX", 1, 2, NoneAddressing),
+
+		newOpCode(0xc8, "INY", 1, 2, NoneAddressing),
+
+		newOpCode(0x4c, "JMP", 3, 3, Absolute),
+		newOpCode(0x6c, "JMP", 3, 5, Absolute),
+
 		newOpCode(0xa9, "LDA", 2, 2, Immediate),
 		newOpCode(0xa5, "LDA", 2, 3, ZeroPage),
 		newOpCode(0xb5, "LDA", 2, 4, ZeroPage_X),
@@ -117,6 +140,58 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 		newOpCode(0xa1, "LDA", 2, 6, Indirect_X),
 		newOpCode(0xb1, "LDA", 2, 5 /* +1 if page crossed */, Indirect_Y),
 
+		newOpCode(0xa2, "LDX", 2, 2, Immediate),
+		newOpCode(0xa6, "LDX", 2, 3, ZeroPage),
+		newOpCode(0xb6, "LDX", 2, 4, ZeroPage_Y),
+		newOpCode(0xae, "LDX", 3, 4, Absolute),
+		newOpCode(0xbe, "LDX", 3, 4 /* +1 if page crossed*/, Absolute_Y),
+
+		newOpCode(0xa0, "LDY", 2, 2, Immediate),
+		newOpCode(0xa4, "LDY", 2, 3, ZeroPage),
+		newOpCode(0xb4, "LDY", 2, 4, ZeroPage_X),
+		newOpCode(0xac, "LDY", 3, 4, Absolute),
+		newOpCode(0xbc, "LDY", 3, 4 /* +1 if page crossed */, Absolute_X),
+
+		newOpCode(0x4a, "LSR", 1, 2, NoneAddressing),
+		newOpCode(0x46, "LSR", 2, 5, ZeroPage),
+		newOpCode(0x56, "LSR", 2, 6, ZeroPage_X),
+		newOpCode(0x4e, "LSR", 3, 6, Absolute),
+		newOpCode(0x5e, "LSR", 3, 7, Absolute_X),
+
+		newOpCode(0x09, "ORA", 2, 2, Immediate),
+		newOpCode(0x05, "ORA", 2, 3, ZeroPage),
+		newOpCode(0x15, "ORA", 2, 4, ZeroPage_X),
+		newOpCode(0x0d, "ORA", 3, 4, Absolute),
+		newOpCode(0x1d, "ORA", 3, 4 /* +1 if page crossed */, Absolute_X),
+		newOpCode(0x19, "ORA", 3, 4 /* +1 if page crossed */, Absolute_Y),
+		newOpCode(0x01, "ORA", 2, 6, Indirect_X),
+		newOpCode(0x11, "ORA", 2, 5 /* +1 if page crossed */, Indirect_Y),
+
+		newOpCode(0x2a, "ROL", 1, 2, NoneAddressing),
+		newOpCode(0x26, "ROL", 2, 5, ZeroPage),
+		newOpCode(0x36, "ROL", 2, 6, ZeroPage_X),
+		newOpCode(0x2e, "ROL", 3, 6, Absolute),
+		newOpCode(0x3e, "ROL", 3, 7, Absolute_X),
+
+		newOpCode(0x6a, "ROR", 1, 2, NoneAddressing),
+		newOpCode(0x66, "ROR", 2, 5, ZeroPage),
+		newOpCode(0x76, "ROR", 2, 6, ZeroPage_X),
+		newOpCode(0x6e, "ROR", 3, 6, Absolute),
+		newOpCode(0x7e, "ROR", 3, 7, Absolute_X),
+
+		newOpCode(0xe9, "SBC", 2, 2, Immediate),
+		newOpCode(0xe5, "SBC", 2, 3, ZeroPage),
+		newOpCode(0xf5, "SBC", 2, 4, ZeroPage_X),
+		newOpCode(0xed, "SBC", 3, 4, Absolute),
+		newOpCode(0xfd, "SBC", 3, 4 /* +1 if page crossed */, Absolute_X),
+		newOpCode(0xf9, "SBC", 3, 4 /* +1 if page crossed */, Absolute_Y),
+		newOpCode(0xe1, "SBC", 2, 6, Indirect_X),
+		newOpCode(0xf1, "SBC", 2, 5 /* +1 if page crossed */, Indirect_Y),
+
+		newOpCode(0x38, "SEC", 1, 2, NoneAddressing),
+		newOpCode(0xf8, "SED", 1, 2, NoneAddressing),
+		newOpCode(0x78, "SEI", 1, 2, NoneAddressing),
+
 		newOpCode(0x85, "STA", 2, 3, ZeroPage),
 		newOpCode(0x95, "STA", 2, 4, ZeroPage_X),
 		newOpCode(0x8d, "STA", 3, 4, Absolute),
@@ -124,6 +199,22 @@ func OpCodesMapFunc() func() map[uint8]*OpCode {
 		newOpCode(0x99, "STA", 3, 5, Absolute_Y),
 		newOpCode(0x81, "STA", 2, 6, Indirect_X),
 		newOpCode(0x91, "STA", 2, 6, Indirect_Y),
+
+		newOpCode(0x86, "STX", 2, 3, ZeroPage),
+		newOpCode(0x96, "STX", 2, 4, ZeroPage_Y),
+		newOpCode(0x8e, "STX", 3, 4, Absolute),
+
+		newOpCode(0x84, "STY", 2, 3, ZeroPage),
+		newOpCode(0x94, "STY", 2, 4, ZeroPage_X),
+		newOpCode(0x8c, "STY", 3, 4, Absolute),
+
+		newOpCode(0xaa, "TAX", 1, 7, NoneAddressing),
+
+		newOpCode(0xa8, "TAY", 1, 2, NoneAddressing),
+
+		newOpCode(0x8a, "TXA", 1, 2, NoneAddressing),
+
+		newOpCode(0x98, "TYA", 1, 2, NoneAddressing),
 	}
 
 	return func() map[uint8]*OpCode {
@@ -191,9 +282,31 @@ func (cpu *CPU) setStatusFlag(mask uint8, set bool) {
 	}
 }
 
-// TODO
+func (cpu *CPU) isStatusFlagSet(mask uint8) bool {
+	return cpu.status&mask != 0
+}
+
+func (cpu *CPU) isStatusFlagClear(mask uint8) bool {
+	return cpu.status&mask == 0
+}
+
 func (cpu *CPU) adc(mode AddressingMode) {
-	return
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+
+	sum := uint16(cpu.register_a) + uint16(val)
+	if cpu.status&CarryFlag != 0 {
+		sum += 1
+	}
+	result := uint8(sum)
+
+	setOverflowFlag := (cpu.register_a^result)&(val^result)&0x80 != 0
+
+	cpu.setStatusFlag(CarryFlag, sum > 0xff)
+	cpu.setStatusFlag(OverflowFlag, setOverflowFlag)
+
+	cpu.register_a = result
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
 }
 
 func (cpu *CPU) and(mode AddressingMode) {
@@ -234,6 +347,16 @@ func (cpu *CPU) bit(mode AddressingMode) {
 	cpu.setStatusFlag(ZeroFlag, val&cpu.register_a == 0)
 }
 
+func (cpu *CPU) branch(shouldBranch bool) {
+	if shouldBranch {
+		displacement := int8(cpu.mem_read(cpu.program_counter))
+		cpu.program_counter += uint16(displacement)
+	}
+}
+
+func (cpu *CPU) brk() {
+	// TODO
+}
 func (cpu *CPU) clc() {
 	cpu.setStatusFlag(CarryFlag, false)
 }
@@ -316,12 +439,166 @@ func (cpu *CPU) inx() {
 	cpu.updateZeroAndNegativeFlags(cpu.register_x)
 }
 
+func (cpu *CPU) iny() {
+	cpu.register_y += 1
+	cpu.updateZeroAndNegativeFlags(cpu.register_y)
+}
+
+func (cpu *CPU) jmp_absolute() {
+	addr := cpu.mem_read_u16(cpu.program_counter)
+	cpu.program_counter = addr
+}
+
+func (cpu *CPU) jmp_indirect() {
+	var jump_addr uint16
+	addr := cpu.mem_read_u16(cpu.program_counter)
+
+	if addr&0xff == 0xff {
+		// accounting for 6502 bug
+		lo := uint16(cpu.mem_read(addr))
+		hi := uint16(cpu.mem_read(addr & 0xff00))
+		jump_addr = (hi << 8) | lo
+	} else {
+		jump_addr = cpu.mem_read_u16(addr)
+	}
+
+	cpu.program_counter = jump_addr
+}
+
 func (cpu *CPU) lda(mode AddressingMode) {
 	addr := cpu.getOperandAddress(mode)
-	val := cpu.mem_read(addr)
-
-	cpu.register_a = val
+	cpu.register_a = cpu.mem_read(addr)
 	cpu.updateZeroAndNegativeFlags(cpu.register_a)
+}
+
+func (cpu *CPU) ldx(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	cpu.register_x = cpu.mem_read(addr)
+	cpu.updateZeroAndNegativeFlags(cpu.register_x)
+}
+
+func (cpu *CPU) ldy(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	cpu.register_y = cpu.mem_read(addr)
+	cpu.updateZeroAndNegativeFlags(cpu.register_y)
+}
+
+func (cpu *CPU) lsr(mode AddressingMode) {
+	var val uint8
+
+	if mode == NoneAddressing {
+		val = cpu.register_a
+		cpu.setStatusFlag(CarryFlag, val&0b0000_0001 != 0)
+		val >>= 1
+		cpu.register_a = val
+	} else {
+		addr := cpu.getOperandAddress(mode)
+		val = cpu.mem_read(addr)
+		cpu.setStatusFlag(CarryFlag, val&0b0000_0001 != 0)
+		val >>= 1
+		cpu.mem_write(addr, val)
+	}
+
+	cpu.updateZeroAndNegativeFlags(val)
+}
+
+func (cpu *CPU) ora(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+	cpu.register_a |= val
+
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
+}
+
+func (cpu *CPU) rol(mode AddressingMode) {
+	var val uint8
+	var setCarryFlag bool
+
+	if mode == NoneAddressing {
+		setCarryFlag = cpu.register_a&0b1000_0000 != 0
+		cpu.register_a <<= 1
+		cpu.register_a |= cpu.status & CarryFlag
+		val = cpu.register_a
+	} else {
+		addr := cpu.getOperandAddress(mode)
+		val := cpu.mem_read(addr)
+		setCarryFlag = val&0b1000_0000 != 0
+		val <<= 1
+		val |= cpu.status & CarryFlag
+		cpu.mem_write(addr, val)
+	}
+
+	cpu.setStatusFlag(CarryFlag, setCarryFlag)
+	cpu.updateZeroAndNegativeFlags(val)
+}
+
+func (cpu *CPU) ror(mode AddressingMode) {
+	var val uint8
+	var setCarryFlag bool
+
+	if mode == NoneAddressing {
+		setCarryFlag = cpu.register_a&0b0000_0001 != 0
+		cpu.register_a >>= 1
+		cpu.register_a |= (cpu.status & CarryFlag) << 7
+		val = cpu.register_a
+	} else {
+		addr := cpu.getOperandAddress(mode)
+		val = cpu.mem_read(addr)
+		setCarryFlag = val&0b0000_0001 != 0
+		val >>= 1
+		val |= (cpu.status & CarryFlag) << 7
+		cpu.mem_write(addr, val)
+	}
+
+	cpu.setStatusFlag(CarryFlag, setCarryFlag)
+	cpu.updateZeroAndNegativeFlags(val)
+}
+
+func (cpu *CPU) sbc(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	val := cpu.mem_read(addr)
+	val = ^val + 1
+
+	sub := uint16(cpu.register_a) + uint16(val) - 1
+	if cpu.status&CarryFlag != 0 {
+		sub += 1
+	}
+	result := uint8(sub)
+
+	setOverflowFlag := (cpu.register_a^result)&(val^result)&0x80 != 0
+
+	cpu.setStatusFlag(CarryFlag, sub > 0xff)
+	cpu.setStatusFlag(OverflowFlag, setOverflowFlag)
+
+	cpu.register_a = result
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
+}
+
+func (cpu *CPU) sec() {
+	cpu.setStatusFlag(CarryFlag, true)
+}
+
+func (cpu *CPU) sed() {
+	cpu.setStatusFlag(DecimalModeFlag, true)
+}
+
+func (cpu *CPU) sei() {
+	cpu.setStatusFlag(InterruptDisableFlag, true)
+}
+
+func (cpu *CPU) sta(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	cpu.mem_write(addr, cpu.register_a)
+}
+
+func (cpu *CPU) stx(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	cpu.mem_write(addr, cpu.register_x)
+}
+
+func (cpu *CPU) sty(mode AddressingMode) {
+	addr := cpu.getOperandAddress(mode)
+	cpu.mem_write(addr, cpu.register_y)
 }
 
 func (cpu *CPU) tax() {
@@ -329,7 +606,17 @@ func (cpu *CPU) tax() {
 	cpu.updateZeroAndNegativeFlags(cpu.register_x)
 }
 
-func (cpu *CPU) sta(mode AddressingMode) {
-	addr := cpu.getOperandAddress(mode)
-	cpu.mem_write(addr, cpu.register_a)
+func (cpu *CPU) tay() {
+	cpu.register_y = cpu.register_a
+	cpu.updateZeroAndNegativeFlags(cpu.register_y)
+}
+
+func (cpu *CPU) txa() {
+	cpu.register_a = cpu.register_x
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
+}
+
+func (cpu *CPU) tya() {
+	cpu.register_a = cpu.register_y
+	cpu.updateZeroAndNegativeFlags(cpu.register_a)
 }
