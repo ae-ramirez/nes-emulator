4e6b1e66464805686f84c834f7aee222a556a76b
Alejandro Ramirez <alejramirez@csumb.edu>
2023-08-13 17:10:57 -0700

    Add files to run nestest
    nestest is a iNes file that can be used to verify that our emulator is
    working as intended
    
    source: https://www.nesdev.org/wiki/Emulator_tests

---
+++ test/nestest/nestest.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"al/nes-emulator/cpu"
+	"al/nes-emulator/rom"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./nestest.nes")
+	if os.IsNotExist(err) {
+		log.Fatal("Could not open nestest.nes")
+	}
+	fstat, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	nestestCode := make([]uint8, fstat.Size())
+	_, err = file.Read(nestestCode)
+	if err != nil {
+		panic(err)
+	}
+
+	c := &cpu.CPU{}
+	rom := &rom.Rom{}
+	if err := rom.Init(nestestCode); err != nil {
+		log.Fatalf("Could not initialize rom: %s", err.Error())
+	}
+	c.Bus.SetRom(rom)
+	c.Reset()
+	c.SetProgramCounter(0xc000)
+
+	c.RunWithCallback(func(c *cpu.CPU) {
+		fmt.Println(cpu.Trace(c))
+	})
+}
